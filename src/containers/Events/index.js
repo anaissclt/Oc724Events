76fef91b826c3ee63950d54fe3b2c19053598c9b
anaissclt <71971929+anaissclt@users.noreply.github.com>
2024-02-21 18:02:43 +0100

    Ajout du filtre de sélection

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,19 +13,24 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
+
   const filteredEvents = (
     (!type
       ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
+      // Filtrer les événements par type
+      :  data?.events.filter((event) => event.type === type)) || []
+  )
+    .filter((event, index) => {
+      if (
+        (currentPage - 1) * PER_PAGE <= index &&
+        PER_PAGE * currentPage > index
+      ) {
+        return true;
+      }
+      return false;
+    });
+
+  
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
